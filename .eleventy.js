7ccb38f968fd0b3eb64cda0f51041134f8713496
iamspruce <iamspruce8@gmail.com>
2021-01-23 21:56:30 +0100

    added

--- .eleventy.js
+++ .eleventy.js
@@ -7,6 +7,7 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPlugin(timeToRead);
     eleventyConfig.addWatchTarget("./src/sass/");
     eleventyConfig.addPassthroughCopy("./src/sass/");
+    eleventyConfig.addPassthroughCopy("./src/assets/");
     eleventyConfig.setTemplateFormats([
         "md",
         "html",

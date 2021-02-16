204cd512cc9a3f410c4b729d13e13abc0a5eb7ca
iamspruce <iamspruce8@gmail.com>
2021-02-17 00:14:06 +0100

    add

---
+++ .eleventy.js
@@ -0,0 +1,60 @@
+const pluginDate = require("eleventy-plugin-date");
+const pluginRss = require("@11ty/eleventy-plugin-rss");
+
+module.exports = function(eleventyConfig) {
+
+    eleventyConfig.setUseGitIgnore(false);
+    eleventyConfig.addPlugin(pluginDate);
+    eleventyConfig.addPlugin(pluginRss);
+    eleventyConfig.addWatchTarget("./src/sass/");
+    eleventyConfig.addPassthroughCopy("./src/sass/");
+    eleventyConfig.addPassthroughCopy("./src/assets/fonts/");
+    eleventyConfig.addPassthroughCopy("./src/assets/img/");
+    eleventyConfig.addPassthroughCopy("./src/assets/js/");
+    eleventyConfig.setTemplateFormats([
+        "md",
+        "html",
+        "liquid",
+        "css",
+        "jpg",
+        "png",
+        "webp",
+        "mp4",
+        "xml",
+        "json",
+        "njk"
+    ]);
+    eleventyConfig.addShortcode("codepen", function(name, title,  hash) {
+        return `<p data-height="370" data-theme-id="light" data-slug-hash="${hash}" data-default-tab="result" data-user="Spruce_khalifa" class="codepen">
+            See the pen 
+            <a href="https://codepen.io/${name}/pen/${hash}/">
+        ${title}
+        </a>, by @${name} on <a href="https://codepen.io">Codepen</a>
+        </p>`
+    });
+    eleventyConfig.addShortcode("image", function(url, alt) {
+        return `<figure class="p-article__img"><picture><source media="(max-width: 799px)" srcset="img/${url}?nf_resize=fit&w=480">
+        <source media="(min-width: 800px)" srcset="img/${url}?nf_resize=fit&w=800">
+        <img src="img/${url}?nf_resize=fit&w=800" alt="${alt}">
+        </picture><figcaption>${alt}</figcaption></figure>`
+    });
+
+    eleventyConfig.addShortcode("caniuse", function(feature) {
+        return `<p class="ciu_embed" data-feature="${feature}" data-periods="future_1,current,past_1,past_2" data-accessible-colours="false">
+        <picture>
+            <source type="image/webp" srcset="https://caniuse.bitsofco.de/image/${feature}.webp">
+            <source type="image/png" srcset="https://caniuse.bitsofco.de/image/${feature}.png">
+            <img src="https://caniuse.bitsofco.de/image/${feature}.jpg" alt="Data on support for the ${feature} feature across the major browsers from caniuse.com">
+        </picture>
+    </p>`
+    });
+
+    return {
+        templateFormats: ["html","md","liquid"],        
+        dir: {
+            input: "src",
+            output: "public"
+        },
+        passthroughFileCopy: true
+    };
+};
---
+++ public/assets/js/theme.js
@@ -0,0 +1,100 @@
+const themeBtn = document.querySelector(".js-theme-btn");
+themeBtn.addEventListener("click", (e) => {
+  const themeWrapper = document.querySelector(".section-theme");
+  themeWrapper.classList.toggle("active");
+});
+
+const themeClose = document.querySelector(".js-close-theme");
+themeClose.addEventListener("click", (e) => {
+  const themeWrapper = document.querySelector(".section-theme");
+  themeWrapper.classList.remove("active");
+});
+
+class CustomTheme {
+  constructor() {
+    this.islocalStorage = function () {
+      try {
+        localStorage.setItem("test", "testing");
+        localStorage.removeItem("test");
+        return true;
+      } catch (error) {
+        return false;
+      }
+    };
+
+    this.schemeBtns = document.querySelectorAll(".js-theme-color");
+    this.schemeBtns.forEach((btn) => {
+      const btnVal = btn.value;
+      btn.addEventListener("click", () => this.themeScheme(btnVal));
+    });
+
+    this.fontBtns = document.querySelectorAll(".js-font-btn");
+    this.fontBtns.forEach((btn) => {
+      const btnVal = btn.value;
+      const btnTag = btn;
+      btn.addEventListener("click", () => this.themeFont(btnVal, btnTag));
+    });
+
+    this.switchBtn = document.querySelector(".js-theme-toggle");
+    const clicked = this.switchBtn;
+    this.switchBtn.addEventListener("click", () => this.themePosition(clicked));
+  }
+
+  themeScheme(btnVal) {
+    document.documentElement.setAttribute("data-theme", btnVal);
+    if (this.islocalStorage) {
+      localStorage.setItem("theme-name", btnVal);
+    }
+  }
+
+  themeFont(btnVal, btnTag) {
+    document.documentElement.style.setProperty("--font-size", `${btnVal}px`);
+    if (this.islocalStorage) {
+      localStorage.setItem("font-size", btnVal);
+    }
+    if (btnVal == localStorage.getItem("font-size")) {
+      removeActive();
+      btnTag.classList.add("active");
+    }
+  }
+
+  themePosition(clicked) {
+    if (clicked.getAttribute("aria-checked") == "true") {
+      clicked.setAttribute("aria-checked", "false");
+      document.documentElement.style.setProperty("--position", "static");
+      document.documentElement.style.setProperty("--top-margin", "0px");
+      if (this.islocalStorage) {
+        localStorage.setItem("position", "static");
+      }
+    } else {
+      clicked.setAttribute("aria-checked", "true");
+      document.documentElement.style.setProperty("--position", "fixed");
+      document.documentElement.style.setProperty("--top-margin", "96px");
+      if (this.islocalStorage) {
+        localStorage.setItem("position", "fixed");
+      }
+    }
+  }
+}
+
+function removeActive() {
+  const btns = document.querySelectorAll(".js-font-btn");
+  btns.forEach((btn) => {
+    btn.classList.remove("active");
+  });
+}
+
+if (window.CSS && CSS.supports("color", "var(--i-support")) {
+  new CustomTheme();
+}
+
+window.addEventListener("load", () => {
+  const fontBtns = document.querySelectorAll(".js-font-btn");
+  fontBtns.forEach((btn) => {
+    const btnVal = btn.value;
+    const btnTag = btn;
+    if (btnVal == localStorage.getItem("font-size")) {
+      btnTag.classList.add("active");
+    }
+  });
+});

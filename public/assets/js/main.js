efab16336f996bcbaa81912001143e03b7894b51
iamspruce <iamspruce8@gmail.com>
2021-03-01 21:28:32 +0100

    commit

---
+++ public/assets/js/main.js
@@ -0,0 +1,15 @@
+var toggleBtn = document.querySelector('.js-nav-open');
+var closeBtn = document.querySelector('.js-nav-close');
+var nav = document.querySelector('.js-nav');
+
+toggleBtn.addEventListener('click', function() {
+    nav.classList.add('active');
+    this.setAttribute("aria-label", "open menu");
+    this.setAttribute("aria-expanded", "false");
+});
+closeBtn.addEventListener('click', function() {
+    nav.classList.remove('active');
+    this.setAttribute("aria-expanded", "true");
+    this.setAttribute("aria-label", "Close menu");
+});
+
